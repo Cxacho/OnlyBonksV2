b57c33a15932eaac77bd24b98f70f8712091de85
Cxacho <m.czachowski1@gmail.com>
2022-03-28 19:49:41 +0200

    Usprawnienie Deck Paneli
    "DeckPanel", "DrawDeckPanel" i "DiscardDeckPanel" usprawniłem o :
    - system skrolowania + rozwijanie panelu przy większej ilości kart
    - przycisk powrotu
    - lepsze ustawienie kart (grid group)

--- Assets/Scripts/Gameplay/DiscardDeck.cs
+++ Assets/Scripts/Gameplay/DiscardDeck.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class DiscardDeck : MonoBehaviour
 {
-    // Start is called before the first frame update
 
     public Transform discardDeckScreen;
     public GameObject discardPanel;
