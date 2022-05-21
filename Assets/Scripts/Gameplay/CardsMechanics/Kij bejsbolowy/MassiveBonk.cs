e5e4fb502b919763604e31a11774aa2228df2958
adriancichy <adrian.cichy.cs@gmail.com>
2022-05-22 00:25:37 +0200

    Squashed commit of the following:
    commit e0d29a6aaf5c8c1a76b5c2408f7e67baca80a853
    Author: Feeeelek <lukaszfelus221@gmail.com>
    Date:   Sat May 14 03:53:52 2022 +0200
    
        dodanie paru kart

---
+++ Assets/Scripts/Gameplay/CardsMechanics/Kij bejsbolowy/MassiveBonk.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MassiveBonk : Card
+{
+    public int attack = 15;
+    private int cost = 3;
+    public GameObject bonk;
+
+
+
+    public override void OnDrop()
+    {
+        gm.checkPlayerMana(cost);
+        if (gm.canPlayCards == true)
+        {
+            base.OnDrop();
+
+            StartCoroutine(ExecuteAfterTime(1f));
+            foreach (Enemy en in _enemies)
+            {
+                if (en.targeted == true)
+                {
+                    en.ReceiveDamage(attack + pl.strenght);
+
+                    en.targeted = false;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("fajnie dzia³a");
+        }
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+
+        //enemy.ReceiveDamage(attack * pl.strenght);
+
+
+        pl.manaText.text = pl.mana.ToString();
+
+
+        /* else
+         {
+             // enemy.currentHealth = 0;
+             gm.state = BattleState.WON;
+             StartCoroutine(gm.OnBattleWin());
+
+         }*/
+
+
+
+
+
+    }
+
+}
+
