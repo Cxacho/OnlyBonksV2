27a8f9308d107ce4dbff8cd315b225519d4bc348
Cxacho <m.czachowski1@gmail.com>
2022-06-08 08:06:45 +0200

    Zmiana value dla wszystkich kart zaimplementowana

--- Assets/Scripts/Gameplay/CardsMechanics/PocketKnifeCard.cs
+++ Assets/Scripts/Gameplay/CardsMechanics/PocketKnifeCard.cs
@@ -1,9 +1,106 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class PocketKnifeCard : Card
 {
+    //Deals 3 damage and applies 3 bleed
+
+    private void Start()
+    {
+
+        desc = $"Deal<color=white>{attack.ToString()}</color> damage and applies 3 bleed";
+
+        this.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = desc;
+    }
+
+
+
+    private void FixedUpdate()
+    {
+
+        PlayerDamageCalculation();
+
+        if (fm.en != null)
+        {
+
+            if (fm.en.vurneable > 0)
+            {
+                kalkulacjaPrzeciwnik = Mathf.RoundToInt(kalkulacja * 1.25f);
+                if (kalkulacjaPrzeciwnik > defaultattack)
+                    desc = $"Deal <color=green>{kalkulacjaPrzeciwnik.ToString()}</color> damage and applies 3 bleed";
+                else if (kalkulacjaPrzeciwnik == defaultattack)
+                    desc = $"Deal <color=white>{kalkulacjaPrzeciwnik.ToString()}</color> damage and applies 3 bleed";
+                else if (kalkulacjaPrzeciwnik < defaultattack)
+                    desc = $"Deal<color=red>{kalkulacjaPrzeciwnik.ToString()}</color> damage and applies 3 bleed";
+            }
+            else
+            {
+                kalkulacjaPrzeciwnik = kalkulacja;
+                if (kalkulacjaPrzeciwnik > defaultattack)
+                    desc = $"Deal <color=green>{kalkulacjaPrzeciwnik.ToString()}</color> damage and applies 3 bleed";
+                else if (kalkulacjaPrzeciwnik == defaultattack)
+                    desc = $"Deal <color=white>{kalkulacjaPrzeciwnik.ToString()}</color> damage and applies 3 bleed";
+                else if (kalkulacjaPrzeciwnik < defaultattack)
+                    desc = $"Deal <color=red>{kalkulacjaPrzeciwnik.ToString()}</color> damage and applies 3 bleed";
+            }
+        }
+        this.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = desc;
+    }
+
+    public void PlayerDamageCalculation()
+    {
+        if (pl.strenght > 0) //gracz ma strenght wiekszy od 0
+        {
+            if (pl.frail > 0) //gracz ma strenght i frail
+            {
+                kalkulacja = Mathf.RoundToInt(((attack + pl.strenght) * 0.75f));
+                if (kalkulacja > defaultattack) // dmg jest wiekszy niz podstawowy 
+                    desc = $"Deal <color=green>{kalkulacja.ToString()}</color> damage and applies 3 bleed";
+
+                else if (kalkulacja == defaultattack) //dmg jest taki sam jak podstawowy
+                    desc = $"Deal <color=white>{kalkulacja.ToString()}</color> damage and applies 3 bleed";
+
+                else if (kalkulacja < defaultattack) //dmg jest mniejszy niz podstawowy 
+                    desc = $"Deal <color=red>{kalkulacja.ToString()}</color> damage and applies 3 bleed";
+            }
+            else //gracz ma strenght ale nie ma fraila 
+            {
+                kalkulacja = (attack + pl.strenght);
+                desc = $"Deal <color=green>{kalkulacja.ToString()}</color> damage and applies 3 bleed";
+            }
+        }
+        else if (pl.strenght == 0) //gracz ma strenght równy 0
+        {
+
+            if (pl.frail > 0) //gracz ma fraila
+            {
+                kalkulacja = Mathf.RoundToInt((attack * 0.75f));
+                desc = $"Deal <color=red>{kalkulacja.ToString()}</color> damage and applies 3 bleed";
+            }
+            else // gracz nie ma fraila 
+            {
+                kalkulacja = attack;
+                desc = $"Deal <color=white>{kalkulacja.ToString()}</color> damage and applies 3 bleed";
+            }
+        }
+        else if (pl.strenght < 0) //gracz ma strenght mniejszy od 0
+        {
+
+            if (pl.frail > 0)    //gracz ma fraila
+            {
+                kalkulacja = Mathf.RoundToInt(((attack + pl.strenght) * 0.75f));
+                desc = $"Deal <color=red>{kalkulacja.ToString()}</color> damage and applies 3 bleed";
+            }
+            else // gracz nie ma fraila 
+            {
+                kalkulacja = (attack + pl.strenght);
+                desc = $"Deal <color=red>{kalkulacja.ToString()}</color> damage and applies 3 bleed";
+            }
+        }
+        this.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = desc;
+    }
+
     public override void OnDrop()
     {
         gm.checkPlayerMana(cost);
