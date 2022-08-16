2fa0f03d91bb1d671b95f4d9b163f39b0563c8c6
Cxacho <m.czachowski1@gmail.com>
2022-08-16 21:10:47 +0200

    CardCollection - New Cards and sorting

--- Assets/Scripts/CardCollection/CardCollectionManager1.cs
+++ Assets/Scripts/CardCollection/CardCollectionManager1.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Linq;
 
 public class CardCollectionManager1 : MonoBehaviour
 {
     // Start is called before the first frame update
     public List<GameObject> cardCollection = new List<GameObject>();
+    public List<GameObject> cardCollectionTemp = new List<GameObject>();
     public GameObject contentPanel;
-    private void Start()
+    private void Awake()
     {
-        for(int i=0; i < cardCollection.Count; i++)
+        
+        SortByMana();
+
+        for (int i=0; i < cardCollection.Count; i++)
         {
-            
-            cardCollection[i].GetComponent<Card>().enabled = false;
-            Instantiate(cardCollection[i], contentPanel.transform);
+
+            Instantiate(cardCollectionTemp[i], contentPanel.transform);
             
         }
+    }
+
+    public void SortByMana()
+    {
+        cardCollectionTemp = cardCollection.OrderBy(t => t.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text).ThenBy(t => t.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text).ToList();
+        Debug.Log(cardCollection[0].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text);
     }
 }
